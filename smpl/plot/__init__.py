b6487b129dfe7d5d58872735b3fd075dcb033910
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-11-16 16:43:50 +0100

    start cleaning code

--- smpl/plot/__init__.py
+++ smpl/plot/__init__.py
@@ -3,5 +3,5 @@ Simplified plotting
 """
 
 from .plot import data,fit,function,unv,usd,auto,plot_kwargs
-__all__ = ['data','fit','function','unv','usd','show','auto','plot_kwargs']
+__all__ = ['data','fit','function','unv','usd','auto','plot_kwargs']
 from matplotlib.pyplot import *
