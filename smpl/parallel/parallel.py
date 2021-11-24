9b4c80d4656198c6a95ba363f3c421cbcf1d1943
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-11-24 19:28:51 +0100

    Update parallel.py

--- smpl/parallel/parallel.py
+++ smpl/parallel/parallel.py
@@ -8,24 +8,25 @@ def queued(q, f, *args, **kwargs):
 
 def res(a):
     """
-        Parallel evaluation of the list generator from :func:`gen`
+    Parallel evaluation of the list generator from :func:`gen`.
 
-        Examples
-        --------
+    Return parallel executed values.
 
-        >>> def twice(x):
-        ...     return x+x
-        >>> for r in [calc(twice,i) for i in range(0,5)]:
-        ...     print(res(r))
-        0
-        2
-        4
-        6
-        8
-        >>> res([calc(lambda x : x**3, i) for i in range(0,5)])
-        [0, 1, 8, 27, 64]
+    Examples
+    --------
+    >>> def twice(x):
+    ...     return x+x
+    >>> for r in [calc(twice,i) for i in range(0,5)]:
+    ...     print(res(r))
+    0
+    2
+    4
+    6
+    8
+    >>> res([calc(lambda x : x**3, i) for i in range(0,5)])
+    [0, 1, 8, 27, 64]
 
-        Return parallel executed values
+        
     """
     if isinstance(a, list):
         return [next(k) for k in a]
@@ -33,9 +34,7 @@ def res(a):
 
 
 def gen(f, *args, **kwargs):
-    """
-        Generates parallel execution list generator
-    """
+    """Generates parallel execution list generator."""
     q = Queue()
     p = Process(target=queued, args=(q, f, *args), kwargs=kwargs)
     yield p.start()
@@ -51,13 +50,12 @@ def calc(f, *args, **kwargs):
 
 def par(f, *args, **kwargs):
     """
-        Parallel execution of f on each element of args and kwargs
+    Parallel execution of f on each element of args and kwargs
 
-        Examples
-        --------
-
-        >>> par(lambda x : x**2, range(0,5))
-        [0, 1, 4, 9, 16]
+    Examples
+    --------
+    >>> par(lambda x : x**2, range(0,5))
+    [0, 1, 4, 9, 16]
 
     """
     return res([calc(f, *[args[k][i] for k in range(len(args))], **{k: v[i] for k, v in kwargs.items()}) for i in range(len(args[0]) if len(args) > 0 else len(next(iter(kwargs.values()))))])
