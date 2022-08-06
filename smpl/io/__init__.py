15c5261ff03917c7b398c14afb05bf5820d92d50
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2022-08-07 00:25:57 +0200

    Fix missing io.read import

--- smpl/io/__init__.py
+++ smpl/io/__init__.py
@@ -1,5 +1,5 @@
 """Simplified input and output."""
 
-from .io import pwd, mkdirs, pr, files, find_file, gf, write
+from .io import pwd, mkdirs, pr, files, find_file, gf, write, read
 
 __all__ = ['pwd', 'mkdirs', 'pr', 'files', 'find_file', 'gf' , 'write','read']
