6374a8904fe73541996d50ee380e3e7b016ca1c3
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2025-05-25 11:30:24 +0200

    add: pytest-mpl

---
+++ tests/mpl/test_smplr.py
@@ -0,0 +1,39 @@
+import pytest
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from smpl import plot
+
+@pytest.mark.mpl_image_compare(baseline_dir="baseline", remove_text=True)
+def test_data():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plot.data(x,y)
+    return plt.gcf()
+
+
+@pytest.mark.mpl_image_compare(baseline_dir="baseline", remove_text=True)
+def test_yscale():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plot.data(x,y, yscale="log")
+    return plt.gcf()
+
+
+
+@pytest.mark.mpl_image_compare(baseline_dir="baseline", remove_text=True)
+def test_xscale():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plot.data(x,y, xscale="log")
+    return plt.gcf()
+
+
+
+@pytest.mark.mpl_image_compare(baseline_dir="baseline", remove_text=True)
+def test_grid():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plot.data(x,y, grid=False)
+    return plt.gcf()
