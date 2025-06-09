d905725704947518f14df8de5e13a81809c40c35
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2025-06-09 13:00:56 +0200

    foramt

--- smpl/plot.py
+++ smpl/plot.py
@@ -1,7 +1,5 @@
 """Simplified plotting."""
 
-import warnings
-
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -275,11 +273,11 @@ def fit(func, *adata, **kwargs):
 
     """
     if "xaxis" in kwargs and not kwargs["xlabel"]:
-        #warnings.warn("xaxis is deprecated. Use xlabel instead.", DeprecationWarning, 2)
+        # warnings.warn("xaxis is deprecated. Use xlabel instead.", DeprecationWarning, 2)
         kwargs["xlabel"] = kwargs["xaxis"]
         # TODO maybe pop
     if "yaxis" in kwargs and not kwargs["ylabel"]:
-        #warnings.warn("yaxis is deprecated. Use ylabel instead.", DeprecationWarning, 2)
+        # warnings.warn("yaxis is deprecated. Use ylabel instead.", DeprecationWarning, 2)
         kwargs["ylabel"] = kwargs["yaxis"]
         # TODO maybe pop
 
